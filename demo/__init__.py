9e4a2a4ace3e06b726e68e270ca782489fe0a286
makora9143 <makora@ht.sfc.keio.ac.jp>
2017-03-26 16:34:26 +0900

    implement front-end

---
+++ demo/__init__.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+from flask import Flask
+from flask import render_template
+from flask import request
+import flask
+import numpy as np
+
+import vae
+
+app = Flask(__name__, static_url_path='/static')
+
+@app.route('/')
+def hello():
+    return render_template('index.html')
+
+@app.route('/api/recon', methods=['POST'])
+def reconstruct():
+    if request.headers['Content-Type'] != 'application/json':
+        print(request.headers['Content-Type'])
+        return flask.jsonify(res='error'), 400
+
+    x = np.array(request.json)
+
+    model = vae.M2VAE()
+
+    y = model.predict(x)
+    z = model.inference(x, y)
+    result = [model.reconstruct(x, y)]
+    result += [model.generate(z, i) for i in range(10)]
+
+    return flask.jsonify(result)
+
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ demo/vae.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+class M2VAE():
+    def predict(self, x):
+        return 1
+
+    def inference(self, x, y):
+        return [0]*78
+
+    def reconstruct(self, x, y):
+        return [0]*784
+
+    def generate(self, z, y):
+        return [255]*784
+
