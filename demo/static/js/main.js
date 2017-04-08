11d3b5afa6bdd90a5969f9cdbb2cc7c7e9abcbfc
makora9143 <makora@ht.sfc.keio.ac.jp>
2017-04-08 19:01:38 +0900

    fix front side

--- demo/static/js/main.js
+++ demo/static/js/main.js
@@ -1,11 +1,13 @@
 /* global $ */
+var row = 32
+var col = 32
 class Main {
     constructor() {
         this.canvas = document.getElementById('main');
         this.input = document.getElementById('input');
         this.recon = document.getElementById('reconstruct');
-        this.canvas.width  = 449; // 16 * 28 + 1
-        this.canvas.height = 449; // 16 * 28 + 1
+        this.canvas.width  = 16 * col + 1; // 16 * 28 + 1
+        this.canvas.height = 16 * row + 1; // 16 * 28 + 1
         this.ctx = this.canvas.getContext('2d');
         this.canvas.addEventListener('mousedown', this.onMouseDown.bind(this));
         this.canvas.addEventListener('mouseup',   this.onMouseUp.bind(this));
@@ -14,20 +16,20 @@ class Main {
     }
     initialize() {
         this.ctx.fillStyle = '#FFFFFF';
-        this.ctx.fillRect(0, 0, 449, 449);
+        this.ctx.fillRect(0, 0, 16*col+1, 16*row+1);
         this.ctx.lineWidth = 1;
-        this.ctx.strokeRect(0, 0, 449, 449);
+        this.ctx.strokeRect(0, 0, 16*col+1, 16*row+1);
         this.ctx.lineWidth = 0.05;
-        for (var i = 0; i < 27; i++) {
+        for (var i = 0; i < row-1; i++) {
             this.ctx.beginPath();
             this.ctx.moveTo((i + 1) * 16,   0);
-            this.ctx.lineTo((i + 1) * 16, 449);
+            this.ctx.lineTo((i + 1) * 16, 16*row+1);
             this.ctx.closePath();
             this.ctx.stroke();
 
             this.ctx.beginPath();
             this.ctx.moveTo(  0, (i + 1) * 16);
-            this.ctx.lineTo(449, (i + 1) * 16);
+            this.ctx.lineTo(16*row+1, (i + 1) * 16);
             this.ctx.closePath();
             this.ctx.stroke();
         }
@@ -68,12 +70,12 @@ class Main {
         img.onload = () => {
             var inputs = [];
             var small = document.createElement('canvas').getContext('2d');
-            small.drawImage(img, 0, 0, img.width, img.height, 0, 0, 28, 28);
-            var data = small.getImageData(0, 0, 28, 28).data;
-            for (var i = 0; i < 28; i++) {
-                for (var j = 0; j < 28; j++) {
-                    var n = 4 * (i * 28 + j);
-                    inputs[i * 28 + j] = (data[n + 0] + data[n + 1] + data[n + 2]) / 3;
+            small.drawImage(img, 0, 0, img.width, img.height, 0, 0, col, row);
+            var data = small.getImageData(0, 0, col, row).data;
+            for (var i = 0; i < row; i++) {
+                for (var j = 0; j < col; j++) {
+                    var n = 4 * (i * row + j);
+                    inputs[i * col + j] = (data[n + 0] + data[n + 1] + data[n + 2]) / 3;
                     ctx.fillStyle = 'rgb(' + [data[n + 0], data[n + 1], data[n + 2]].join(',') + ')';
                     ctx.fillRect(j * 5, i * 5, 5, 5);
                 }
@@ -108,9 +110,9 @@ class Main {
 
 function draw_digit(canvas, data) {
     var ctx = canvas.getContext('2d')
-    for (var i = 0; i < 28; i++) {
-        for (var j = 0; j < 28; j++) {
-            ctx.fillStyle = 'rgb(' + [data[i*28+j], data[i*28+j], data[i*28+j]].join(',') + ')';
+    for (var i = 0; i < row; i++) {
+        for (var j = 0; j < col; j++) {
+            ctx.fillStyle = 'rgb(' + [data[i*row+j], data[i*row+j], data[i*row+j]].join(',') + ')';
             ctx.fillRect(j * 5, i * 5, 5, 5);
         }
     }
